a2378c95fc1c135e00b6401c858d74debe5101de
Leandro308 <99918172+Leandro308@users.noreply.github.com>
2023-11-16 07:31:40 -0300

    Add files via upload

---
+++ eventos/mouse/index.js
@@ -0,0 +1,9 @@
+function acao1(){
+    alert(`Evento Disparado`)
+    
+}
+
+function acao2(){
+    alert(`Evento 2 Disparado`)
+    
+}
---
+++ eventos/navegador/app.js
@@ -0,0 +1,7 @@
+function acao(){
+    console.log(`Eventos Disparados`)
+}
+
+function acao1(){
+    console.log(`Eventos Onscroll Disparados`)
+}
